814829153943c993e95e353695283c91920af1f6
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-11-24 22:55:12 +0300

    Добавьте файлы проекта.

---
+++ WPF14/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF14;
+
+namespace WPF14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> product;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            product = new ObservableCollection<Product>();
+            product.Add(new Product()
+            {
+                Name = "Coca-Cola",
+                Price = 10,
+                Image = "Data/Coca-cola.png",
+                Category = ProductTypes.Food
+            });
+
+            listBox.ItemsSource = product;
+        }
+    }
+}
---
+++ WPF14/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF14
+{
+    public enum ProductTypes
+    {
+        Food,
+        Tech
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public ProductTypes Category { get; set; }
+
+        //public Product(string name, int price, string image, ProductTypes category)
+        //{
+        //    Name = name;
+        //    Price = price;
+        //    Image = image;
+        //    Category = category;
+        //}
+    }
+}
